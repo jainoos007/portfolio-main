fdc5f3e01807eac84a794ef83bdedeaf151d8493
jainoos <ammjainoos@gmail.com>
2026-04-14 12:55:28 +0530

    update hero component to link to resume and improve command bar styling

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,20 +1,21 @@
 import pfpImg from "../assets/pfp.png";
 
 export default function Hero() {
-  const copy = () => {
-    navigator.clipboard.writeText("npx jainoos");
-  };
-
   return (
     <header className="hero-container">
       <img src={pfpImg} className="profile-img" alt="Avatar" />
       <div className="hero-info">
         <h1>Hi, I'm Jainoos</h1>
         {/* <p className="tagline">I mess with code, break stuff, and make it better</p> */}
-        <div className="command-bar" onClick={copy}>
+        <a
+          href="resume_jainoos.pdf"
+          target="_blank"
+          rel="noreferrer"
+          className="command-bar"
+        >
           <span className="neural-nodes"></span>
-          <code className="outlier-text">aka overthinker</code>
-        </div>
+          <code className="outlier-text">Download Resume</code>
+        </a>
       </div>
     </header>
   );
